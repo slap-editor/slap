44f7b4dbe99b5095ac6af6606c7c79ac3b3341a9
Dan Kaplun <dan@beardtree.com>
2014-05-05 07:07:15 -0500

    Renames markup.markupLine to markup, markup.escapeCurlies to markup.escape, markup.markupIndex to markup.index, markup.removeMarkup to markup.strip

--- test/markup.js
+++ test/markup.js
@@ -5,30 +5,30 @@ var test = require('tape');
 var markup = require('../lib/markup');
 
 test("markup", function (t) {
-  t.test(".markupIndex", function (st) {
+  t.test(".index", function (st) {
     st.test("should treat {open} and {close} as single characters", function (sst) {
       sst.plan(3);
 
       var str = "Curlies{open}!{close}";
-      sst.equal(str.slice(markup.markupIndex(str, 7)), "{open}!{close}");
-      sst.equal(str.slice(markup.markupIndex(str, 8)), "!{close}");
-      sst.equal(str.slice(markup.markupIndex(str, 9)), "{close}");
+      sst.equal(str.slice(markup.index(str, 7)), "{open}!{close}");
+      sst.equal(str.slice(markup.index(str, 8)), "!{close}");
+      sst.equal(str.slice(markup.index(str, 9)), "{close}");
     });
     st.test("should get indices correctly", function (sst) {
       sst.plan(4);
 
       var str = "{inverse}Markup {green-fg}{/green-fg}{/inverse}{bold}.{/bold}";
-      sst.equal(markup.markupIndex(str, 0), 0);
-      sst.equal(markup.markupIndex(str, 1), 10);
-      sst.equal(str.slice(markup.markupIndex(str, 7)), "{bold}.{/bold}");
-      sst.equal(markup.markupIndex(str, Infinity), str.length);
+      sst.equal(markup.index(str, 0), 0);
+      sst.equal(markup.index(str, 1), 10);
+      sst.equal(str.slice(markup.index(str, 7)), "{bold}.{/bold}");
+      sst.equal(markup.index(str, Infinity), str.length);
     });
   });
-  t.test(".escapeCurlies", function (st) {
+  t.test(".escape", function (st) {
     st.test("should escape curlies", function (sst) {
       sst.plan(1);
 
-      sst.equal(markup.escapeCurlies("open: {, close: }"), "open: {open}, close: {close}");
+      sst.equal(markup.escape("open: {, close: }"), "open: {open}, close: {close}");
     });
   });
 });
