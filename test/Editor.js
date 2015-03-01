f48d5680fc12fa059e4812bce1d8c462dd43d7ce
Dan Kaplun <dan@secrettriangle.net>
2015-03-01 16:44:10 -0500

    Fixes Editor test semantics

--- test/Editor.js
+++ test/Editor.js
@@ -14,12 +14,19 @@ module.exports = function (t) {
       st.test(".open", function (sst) {
         sst.skip("should open a file with perms 000 correctly", function (ssst) {
           ssst.plan(1);
-    
-          var perms000File = 'fixtures/perms-000';
-          fs.chmodSync(perms000File, '000'); // can't be checked in with 000 perms
-          editor.open(perms000File).done(function () {
-            ssst.equal(editor.textBuf.getText(), '');
-          });
+
+          var perms000File = '/Users/dan/stuff/slap/test/fixtures/perms-000';
+
+          // can't be checked in with 000 perms
+          var originalPerms = (fs.statSync(perms000File).mode.toString(8).match(/[0-7]{3}$/) || [])[0] || '644';
+          fs.chmodSync(perms000File, '000');
+
+          editor.open(perms000File)
+            .then(function () {
+              ssst.equal(editor.textBuf.getText(), '');
+            })
+            .finally(function () { fs.chmodSync(perms000File, originalPerms); })
+            .done();
         });
         sst.end();
       });
