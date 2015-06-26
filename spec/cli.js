5ff06faabe13fc1878ed7e1595d12fc5153a42db
Dan Kaplun <dbkaplun@gmail.com>
2015-06-26 00:26:57 -0400

    fix(tests): detach pane on quit instead of child editor
    remove through2, not needed anymore
    move test/ -> spec/
    use base-widget/spec/util

--- spec/cli.js
+++ spec/cli.js
@@ -3,21 +3,13 @@
 
 var test = require('tape');
 var Promise = require('bluebird');
-var through2 = require('through2');
-var fs = require('fs');
+var util = require('base-widget/spec/util');
 
 var cli = require('../lib/cli');
 var Slap = require('../lib/ui/Slap');
 
 test("cli", function (t) {
-  var input = through2();
-  input.setRawMode = function () {};
-  input.isTTY = true;
-
-  var output = through2();
-  output.isTTY = true;
-
-  Promise.using(cli({input: input, output: output}), function (slap) {
+  Promise.using(cli({screen: util.screenFactory()}), function (slap) {
     t.test("should create an instance of slap", function (st) {
       st.plan(1);
 
